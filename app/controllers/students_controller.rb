375a12e797c6a925133a540bcf73ed0ee0395aeb
lumrachele <rlum894@gmail.com>
2018-12-15 17:03:10 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,35 @@
+#require "byebug"
+
 class StudentsController < ApplicationController
-  
+  before_action :find_student, only: [:show]
+
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+    find_student
   end
+
+  def new
+    Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.permit(:first_name, :last_name)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  #get 'students/new', to: 'students#new', as: 'new_student'
+  #get 'students/create', to: 'students#create', as: 'create_student'
+  resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
 end
